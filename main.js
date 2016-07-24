4b72a7855c439cb15775dfc60c898461c03bfe32
Kendal Cockrel <kendal_cockrel@brown.edu>
2016-07-23 23:59:03 -0700

    add mongo lines

--- main.js
+++ main.js
@@ -280,28 +280,18 @@ Datamap.prototype._handleMapReady = function(datamap) {
 
 new Datamap();
 
+mongoData();
 function mongoData() {
 // Retrieve
 var MongoClient = require('mongodb').MongoClient;
 
 // Connect to the db
-MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+MongoClient.connect("mongodb://heroku_2mm2czgg:go0i1j0kiqpdasta9k7dh5899b@ds015700.mlab.com:15700/heroku_2mm2czgg", function(err, db) {
   if(err) { return console.dir(err); }
 
-  var collection = db.collection('test');
-  var docs = [{mykey:1}, {mykey:2}, {mykey:3}];
+  var collection = db.collection('districts');
+  collection.find().toArray(function(err, items) {});
 
-  collection.insert(docs, {w:1}, function(err, result) {
-
-    collection.find().toArray(function(err, items) {});
-
-    var stream = collection.find({mykey:{$ne:2}}).stream();
-    stream.on("data", function(item) {});
-    stream.on("end", function() {});
-
-    collection.findOne({mykey:1}, function(err, item) {});
-
-  });
 });
 
 }
